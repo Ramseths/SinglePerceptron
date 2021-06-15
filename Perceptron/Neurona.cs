493599c5167edefc235a86016832b212e5fa29db
Ramseths <juan.ramseths@gmail.com>
2021-06-15 14:15:17 -0500

    Aplicando AND a la Neurona

--- Perceptron/Neurona.cs
+++ Perceptron/Neurona.cs
@@ -6,19 +6,8 @@ namespace Perceptron
 {
     class Neurona
     {
-        double w1, w2, x1, x2, umbral;
-        // Constructor para iniciar la neurona
-        public Neurona(double x1, double x2, double w1, double w2, double umbral)
-        {
-            this.x1 = x1;
-            this.x2 = x2;
-            this.w1 = w1;
-            this.w2 = w2;
-            this.umbral = umbral;
-        }
-
         // Método para devolver el valor de la neurona
-        public double Valor()
+        public static double NeuronaValor(double x1, double x2, double w1, double w2, double umbral)
         {
             return umbral + x1 * w1 + x2 * w2;
         }
--- Perceptron/Program.cs
+++ Perceptron/Program.cs
@@ -17,19 +17,51 @@ namespace Perceptron
             {
                 state = true;
 
-                w1 = r.NextDouble();
-                w2 = r.NextDouble();
-                umbral = r.NextDouble();
+                w1 = r.NextDouble() - r.NextDouble();
+                w2 = r.NextDouble() - r.NextDouble();
+                umbral = r.NextDouble() - r.NextDouble();
+
+                // Resultados
+                Console.WriteLine("\n ###################");
+                Console.WriteLine("W1 = {0}", w1);
+                Console.WriteLine("W2 = {0}", w2);
+                Console.WriteLine("Umbral = {0}", umbral);
+                Console.WriteLine("X. Valor = 1 | 1 = {0}", sigmoide(Neurona.NeuronaValor(1, 1, w1, w2, umbral)));
+                Console.WriteLine("X. Valor = 1 | 0 = {0}", sigmoide(Neurona.NeuronaValor(1, 0, w1, w2, umbral)));
+                Console.WriteLine("X. Valor = 0 | 1 = {0}", sigmoide(Neurona.NeuronaValor(0, 1, w1, w2, umbral)));
+                Console.WriteLine("X. Valor = 0 | 0 = {0}", sigmoide(Neurona.NeuronaValor(0, 0, w1, w2, umbral)));
+
+                // Primera comprobación AND 1 | 1 = 1
+                if (sigmoide(Neurona.NeuronaValor(1, 1, w1, w2, umbral)) != 1)
+                {
+                    state = false;
+                }
+
+                // Segunda comprobación AND 1 | 0 = 0
+                if (sigmoide(Neurona.NeuronaValor(1, 0, w1, w2, umbral)) != 0)
+                {
+                    state = false;
+                }
+
+                // Tercera comprobación AND 0 | 1 = 0
+                if (sigmoide(Neurona.NeuronaValor(0, 1, w1, w2, umbral)) != 0)
+                {
+                    state = false;
+                }
+
+                // Cuarta comprobación AND 0 | 0 = 0
+                if (sigmoide(Neurona.NeuronaValor(0, 0, w1, w2, umbral)) != 0)
+                {
+                    state = false;
+                }
 
             }
-
         }
 
-        public static double sigmoide(float rl)
+        public static double sigmoide(double rl)
         {
             return rl > 0 ? 1 : 0;
         }
-
 
     }
 }
