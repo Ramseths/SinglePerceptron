57979f84831988f0241bc39e2199bd10b9b5b0bd
Ramseths <juan.ramseths@gmail.com>
2021-06-15 13:47:21 -0500

    Agregando neurona

---
+++ Perceptron/Neurona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Perceptron
+{
+    class Neurona
+    {
+        // Constructor para iniciar la neurona
+        public Neurona(double x1, double x2, double w1, double w2, double umbral)
+        {
+            
+        }
+    }
+}
--- Perceptron/Program.cs
+++ Perceptron/Program.cs
@@ -6,13 +6,28 @@ namespace Perceptron
     {
         static void Main(string[] args)
         {
-            
+            // Weights de la ANN
+            double w1, w2;
+            double umbral;
+
+            Random r = new Random();
+            bool state = false;
+            // Comprobar los weights
+            while (!state)
+            {
+                state = true;
+
+                w1 = r.NextDouble();
+                w2 = r.NextDouble();
+                umbral = r.NextDouble();
+
+            }
+
         }
 
-        //Funcion de activación
-        public float funcion(float d)
+        public static double sigmoide(float rl)
         {
-            return d > 0 ? 1 : 0;
+            return rl > 0 ? 1 : 0;
         }
     }
 }
