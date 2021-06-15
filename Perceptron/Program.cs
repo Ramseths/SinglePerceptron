8c8a9a40bfd21386cd0ed4b688352952694d9de2
Ramseths <juan.ramseths@gmail.com>
2021-06-14 21:05:22 -0500

    Creando funcion

---
+++ Perceptron/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Perceptron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        //Funcion de activación
+        public float funcion(float d)
+        {
+            return d > 0 ? 1 : 0;
+        }
+    }
+}
